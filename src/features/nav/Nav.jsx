ac78c876ead7affed74bd3d49b2ebaa0341eb588
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2021-07-22 13:38:47 +0530

    feat: added nav menu funcitionality

--- src/features/nav/Nav.jsx
+++ src/features/nav/Nav.jsx
@@ -1,53 +1,64 @@
 import navStyles from "./Nav.module.css";
-import { NavLink } from "react-router-dom";
-import { FiHome, FiEdit } from "react-icons/fi";
+import { NavLink, Link } from "react-router-dom";
+import { FiEdit } from "react-icons/fi";
+import { AiFillHome } from "react-icons/ai";
 import { FaUserAlt, FaRegBell } from "react-icons/fa";
 
 export const Nav = () => {
 	return (
 		<div className={navStyles.container}>
 			<header className={navStyles.head_container}>
-				<h1>instaCam</h1>
+				<Link to="/" className={navStyles.head_logo}>
+					<h1>instaCam</h1>
+				</Link>
 			</header>
-			<section className={navStyles.link_container}>
-				<nav>
-					<ul>
-						<li>
-							<NavLink to="/">
-								<FiHome />
-								Home
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/">
-								<FaRegBell />
-								Notification
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/user-profile">
-								<FaUserAlt />
-								Profile
-							</NavLink>
-						</li>
-						<li>
-							<NavLink to="/display">
-								<FiEdit />
-								Display
-							</NavLink>
-						</li>
-						<li>
-							<button>
-								<NavLink to="/display">Logout</NavLink>
-							</button>
-						</li>
-					</ul>
-				</nav>
-			</section>
+
+			<nav className={navStyles.link_container}>
+				<ul className={navStyles.link_list}>
+					<li>
+						<NavLink to="/" className={navStyles.links}>
+							<AiFillHome className={navStyles.link_icon} />
+							Home
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/" className={navStyles.links}>
+							<FaRegBell className={navStyles.link_icon} />
+							Notification
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/user-profile" className={navStyles.links}>
+							<FaUserAlt className={navStyles.link_icon} />
+							Profile
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to="/display" className={navStyles.links}>
+							<FiEdit className={navStyles.link_icon} />
+							Display
+						</NavLink>
+					</li>
+					<li>
+						<button className={navStyles.link_logout_cta}>
+							<NavLink to="/display">Logout</NavLink>
+						</button>
+					</li>
+				</ul>
+			</nav>
+
 			<footer className={navStyles.footer_container}>
-				<button>
-					<NavLink to="/user-profile">Profile</NavLink>
-				</button>
+				<NavLink to="/user-profile" className={navStyles.nav_user}>
+					<img
+						src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+						alt="user_img"
+						className={navStyles.nav_user_img}
+					/>
+					<article className={navStyles.nav_user_text}>
+						<h1>Utsav Kumar</h1>
+						<h2>@utsavkumar280</h2>
+					</article>
+				</NavLink>
 			</footer>
 		</div>
 	);
