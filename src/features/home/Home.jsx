4dee6b0ed12aa494e5658f70c2a66491564ec7dc
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2021-08-15 15:29:02 +0530

    feat: added postcards

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,5 +1,5 @@
 import homeStyles from "./Home.module.css";
-import { AddPost } from "./AddPost";
+import { AddPost } from "./AddPost/index";
 import { PostList } from "./PostList";
 
 export const Home = () => {
@@ -9,8 +9,8 @@ export const Home = () => {
 				<p className={homeStyles.route_head}>Home</p>
 			</header>
 			<AddPost />
-
 			<PostList />
+			<div className={homeStyles.home_end}> </div>
 		</div>
 	);
 };
--- src/features/home/PostList.jsx
+++ src/features/home/PostList.jsx
@@ -1,18 +1,11 @@
 import homeStyles from "./Home.module.css";
+import { PostCard } from "./PostCard";
 
 export const PostList = () => {
 	return (
 		<section className={homeStyles.postList}>
-			<div className={`${homeStyles.main_post} ${homeStyles.post_layout}`}>
-				<section className={homeStyles.user_pic_container}>
-					<img
-						src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-						alt="post_user_pic"
-						className={homeStyles.user_pic}
-					/>
-				</section>
-				<section className={homeStyles.post_content}></section>
-			</div>
+			<PostCard />
+			<PostCard />
 		</section>
 	);
 };
