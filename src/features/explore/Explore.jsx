453b6891b379e38c91661eadea58112637c90999
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-22 12:30:19 +0530

    refact: changed user's list logic

--- src/features/explore/Explore.jsx
+++ src/features/explore/Explore.jsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import exploreStyles from "./Explore.module.css";
 import { FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
 export const Explore = () => {
+	const [inputSearch, setInputSearch] = useState("");
 	return (
 		<div className={exploreStyles.container}>
 			<header className={exploreStyles.head_container}>
@@ -15,204 +17,115 @@ export const Explore = () => {
 						type="text"
 						className={exploreStyles.search_input}
 						placeholder="Search users"
+						value={inputSearch}
+						onChange={(e) => setInputSearch(e.target.value)}
 					/>
 				</div>
 			</header>
-			<section className={exploreStyles.suggested_section}>
-				<div className={exploreStyles.suggested_container}>
-					<header className={exploreStyles.suggested_header}>
-						<p>Users you might like</p>
-					</header>
-					<section className={exploreStyles.users_list}>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-						<Link to="/user-profile" className={exploreStyles.users}>
-							<img
-								src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
-								alt="post_user_pic"
-								className={exploreStyles.users_pic}
-							/>
-							<section className={exploreStyles.users_info}>
-								<h1>Utsav Kumar</h1>
-								<h2>@utsavkumar280</h2>
-							</section>
-						</Link>
-					</section>
-				</div>
+
+			<section className={exploreStyles.userList_section}>
+				{inputSearch ? (
+					<div className={exploreStyles.userList_container_alt}>
+						<section className={exploreStyles.users_list_alt}>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+						</section>
+					</div>
+				) : (
+					<div className={exploreStyles.userList_container}>
+						<header className={exploreStyles.userList_header}>
+							<p>Users you might like</p>
+						</header>
+						<section className={exploreStyles.users_list}>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+							<Link to="/user-profile" className={exploreStyles.users}>
+								<img
+									src="https://i.postimg.cc/gJPZNW57/mini-passport-pic.jpg"
+									alt="post_user_pic"
+									className={exploreStyles.users_pic}
+								/>
+								<section className={exploreStyles.users_info}>
+									<h1>Utsav Kumar</h1>
+									<h2>@utsavkumar280</h2>
+								</section>
+							</Link>
+						</section>
+					</div>
+				)}
 			</section>
 		</div>
 	);
