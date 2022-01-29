35eef8f14d675324fb02d906cce77e81cb0e0dd3
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-01-30 02:35:43 +0530

    feat: initializing upload in addPost

--- src/features/home/Home.jsx
+++ src/features/home/Home.jsx
@@ -1,16 +1,33 @@
+import { useState } from "react";
 import homeStyles from "./Home.module.css";
 import { AddPost } from "./AddPost/index";
 import { PostList } from "./PostList";
+import PreviewModal from "../authentication/profile/PreviewModal";
 
 export const Home = () => {
+	const [postImgPreview, setPostImgPreview] = useState("");
+	const [postImgFile, setPostImgFile] = useState(null);
 	return (
 		<div className={homeStyles.container}>
 			<header className={homeStyles.head_container}>
 				<p className={homeStyles.route_head}>Home</p>
 			</header>
-			<AddPost />
+			<AddPost
+				postImgPreview={postImgPreview}
+				setPostImgPreview={setPostImgPreview}
+				postImgFile={postImgFile}
+				setPostImgFile={setPostImgFile}
+			/>
 			<PostList />
 			<div className={homeStyles.home_end} />
+			{postImgPreview && (
+				<PreviewModal
+					preview={postImgPreview}
+					setPreview={setPostImgPreview}
+					file={postImgFile}
+					setFile={setPostImgFile}
+				/>
+			)}
 		</div>
 	);
 };
