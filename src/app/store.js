9f8145fcb0619155ce9161989d7f0d4fbb848d74
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-02-08 14:56:08 +0530

    feat: added all the reducers

--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postsReducer from "../features/posts/postsSlice";
+
+import authReducer from "../features/authentication/authSlice";
+import profileReducer from "../features/authentication/profile/profieSlice";
+import followersReducer from "../features/authentication/profile/followerUsers/followersSlice";
+import followingsReducer from "../features/authentication/profile/followingUsers/followingsSlice";
+import postsReducer from "../features/home/posts/postsSlice";
 import displayReducer from "../features/display/displaySlice";
 
 export const store = configureStore({
 	reducer: {
+		auth: authReducer,
+		profile: profileReducer,
+		followers: followersReducer,
+		followings: followingsReducer,
 		posts: postsReducer,
 		display: displayReducer,
 	},
