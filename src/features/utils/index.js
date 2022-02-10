2bd39b583306f0da632181b6886d3097969abd8b
Utsav Kumar <66728108+utsavkumar-280@users.noreply.github.com>
2022-02-10 15:13:54 +0530

    feat: added test credentials

--- src/features/utils/index.js
+++ src/features/utils/index.js
@@ -1,5 +1,5 @@
 let local;
-local = "http://localhost:8080";
+//local = "http://localhost:8080";
 export const API_URL = local || "https://cine-insta.herokuapp.com";
 
 export const CLOUDINARY_URL = "https://api.cloudinary.com/v1_1/df5ippkko";
